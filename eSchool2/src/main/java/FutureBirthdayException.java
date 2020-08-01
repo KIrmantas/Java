ec32e09d831419eced067f041f737e93e6084d21
Irmantas Kaunietis <kirmantas74@gmail.com>
2020-08-01 14:59:35 +0300

    Day 1 example of person

--- eSchool2/src/main/java/FutureBirthdayException.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-
-public class FutureBirthdayException extends RuntimeException{
-    public FutureBirthdayException(LocalDate futureDate, String argumentName) {
-        super(argumentName + " cannot be future. Was " + futureDate);
-    }
-}
